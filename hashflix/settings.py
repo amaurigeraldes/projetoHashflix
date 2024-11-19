d0fff597e1c94437009a7f7d145ac065025b7fa2
Amaurí Geraldes <amaurimgeraldes@gmail.com>
2024-11-19 18:09:07 -0300

    Debug para True em Else

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -51,7 +51,7 @@ if os.getenv("PRODUCTION_DEBUG"):
     DEBUG = False
 else:
     # Local impact: Modify this constant!
-    DEBUG = False
+    DEBUG = True
 if DEBUG:
     print("⚠️ Warning: LOCAL DEBUG Mode is Active!!")
 
